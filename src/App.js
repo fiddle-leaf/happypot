190e79bb18dac95913b15017e85dc0d851374422
Shanon Palomino Salinas <ssalinas@cfpboard.org>
2024-07-15 19:10:49 -0400

    Edit package.json file with proxy address for development

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './pages/Home';
+import Home from './pages/home/Home';
+import Auth from './pages/home/auth/Auth';
 
 function App() {
   return (
     <div className="App">
-    
-      <Home />
+      <Auth />
+      {/*<Home />*/}
      
     </div>
   );
